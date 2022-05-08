8ebeee56fd13f5c1e2fdea84f96558e54dd00d1d
mahdyfo <mahdyfof@gmail.com>
2022-05-08 18:02:29 +0200

    Create simulate example file

---
+++ simulate.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Person;
+
+require_once 'vendor/autoload.php';
+
+$person = new Person();
+$person->emotion->setJoy(40);
+$person->emotion->setTrust(60);
+$person->emotion->setFear(50);
+$person->emotion->setSurprise(15);
+$person->emotion->setSadness(80);
+$person->emotion->setDisgust(60);
+$person->emotion->setAnger(60);
+$person->emotion->setAnticipation(85);
+
+var_dump($person->emotion->getSubEmotions());
